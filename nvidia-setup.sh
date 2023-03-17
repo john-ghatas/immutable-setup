ae27c9557a8a7af1e1bd68cd7e7d3fcce74ecc88
John Ghatas <johnghatas97@hotmail.com>
2023-03-17 17:31:52 +0100

    feat(nvidia): Add NVENC driver install

--- nvidia-setup.sh
+++ nvidia-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setting up the NVIDIA drivers"
 rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia
-rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia-cuda
+rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs
 rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1 initcall_blacklist=simpledrm_platform_driver_init
 
 echo "Please run the following ostree command if you're installing silverblue on an NVIDIA only system"
