997cf50f31514b5c8d2951e09291721c0e343b03
John Ghatas <johnghatas97@hotmail.com>
2023-03-20 14:09:18 +0100

    feat: Merge both upgrade wrappers into one

--- upgrade.sh
+++ upgrade.sh
@@ -3,6 +3,7 @@ source /etc/os-release
 
 read -p "What version of Fedora are you upgrading to? (37, 38 etc): " NEXT_VERSION
 echo "Upgrading to Fedora $NEXT_VERSION from Fedora $VERSION_ID"
-read -p "Press any key to continue...." 
+read -p "Press any key to continue...."
 
+rpm-ostree update --uninstall rpmfusion-free-release --uninstall rpmfusion-nonfree-release --install rpmfusion-free-release --install rpmfusion-nonfree-release
 sudo rpm-ostree rebase fedora:fedora/$NEXT_VERSION/x86_64/$VARIANT_ID 
