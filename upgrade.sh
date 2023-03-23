f8e938407b249b1a092944301b601fa8d0316024
John Ghatas <johnghatas97@hotmail.com>
2023-03-23 17:30:36 +0100

    feat: Remove sudo from the upgrade scripts

--- upgrade.sh
+++ upgrade.sh
@@ -5,4 +5,4 @@ read -p "What version of Fedora are you upgrading to? (37, 38 etc): " NEXT_VERSI
 echo "Upgrading to Fedora $NEXT_VERSION from Fedora $VERSION_ID"
 read -p "Press any key to continue...." 
 
-sudo rpm-ostree rebase fedora:fedora/$NEXT_VERSION/x86_64/$VARIANT_ID 
+rpm-ostree rebase fedora:fedora/$NEXT_VERSION/x86_64/$VARIANT_ID 
