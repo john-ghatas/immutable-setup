23b9bbb7a70409cd5786b1a302ac7d4362a42122
John Ghatas <johnghatas97@hotmail.com>
2023-04-07 19:18:22 +0200

    scripts: Replace distrobox with the repo version

--- post-reboot.sh
+++ post-reboot.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Setting up packages" 
-rpm-ostree install distrobox docker ffmpeg gnome-tweaks gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi neovim tilix zsh 
+rpm-ostree install docker ffmpeg gnome-tweaks gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi neovim tilix zsh 
 rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld
 rpm-ostree update --uninstall rpmfusion-free-release --uninstall rpmfusion-nonfree-release --install rpmfusion-free-release --install rpmfusion-nonfree-release
+
+# Install repo version of distrobox 
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
 
 echo "Install the hardware codecs for your system"
 echo "AMD: rpm-ostree override remove mesa-vdpau-drivers --install mesa-vdpau-drivers-freeworld"
