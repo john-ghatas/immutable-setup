aa42f070fed47082b34021f069a4abd131943444
John Ghatas <johnghatas97@hotmail.com>
2023-05-05 22:09:46 +0200

    ublue: Enable the RPMFusion repos for layering extra packages

--- post-reboot.sh
+++ post-reboot.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+e!/bin/bash
 echo "Setting up packages" 
 rpm-ostree install android-tools docker gnome-shell-extension-user-theme neovim tilix zsh
 
 # Install repo version of distrobox 
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
+
+# Enable the rpmfusion repos
+sudo sed -i "s/enabled=0/enabled=1/" /etc/yum.repos.d/rpmfusion-{free,nonfree}{.repo,-updates.repo}
 
 echo "Please reboot again to make the changes final" 
