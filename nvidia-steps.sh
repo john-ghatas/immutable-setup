9edaca2710778c4065f6f7f55a4064438041c75f
John Ghatas <johnghatas97@hotmail.com>
2023-05-15 20:00:14 +0200

    ublue: Extend the nvidia scripts

--- nvidia-steps.sh
+++ nvidia-steps.sh
@@ -3,5 +3,6 @@ rpm-ostree kargs \
     --append=rd.driver.blacklist=nouveau \
     --append=modprobe.blacklist=nouveau \
     --append=nvidia-drm.modeset=1
+sudo systemctl enable --now supergfxd
 
-echo "If you want secure boot to work head to https://ublue.it/images/nvidia/ and follow the steps to import the layered certificate."
+echo "If you want secure boot to work head to https://ublue.it/images/nvidia/ and follow the steps to create the layered certificate. Enroll the MOK cert in the first reboot attempt after creating the key."
