dee62f22a31ad8819d4b31d811ee2149232c6ade
John Ghatas <johnghatas97@hotmail.com>
2023-05-20 12:20:56 +0200

    scripts: Enable the podman API service and the TRIMFS service

--- post-reboot.sh
+++ post-reboot.sh
@@ -2,6 +2,10 @@ e!/bin/bash
 echo "Setting up packages" 
 rpm-ostree install android-tools docker gnome-shell-extension-user-theme neovim tilix zsh
 
+# Enabling the podman services and the TRIM service
+sudo systemctl enable --now podman.socket
+sudo systemctl enable --now fstrim.timer
+
 # Install repo version of distrobox 
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
 
