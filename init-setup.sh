2becaf6ae06ea5a62bd43f1f56e56f36a0ab840b
John Ghatas <johnghatas97@hotmail.com>
2023-06-09 18:16:49 +0200

    fix: Add sudo for a chmod operation on udev rules

--- init-setup.sh
+++ init-setup.sh
@@ -6,7 +6,7 @@ flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flath
 flatpak override --user --filesystem=$HOME/.themes --filesystem=$HOME/.icons --filesystem=$HOME/.config/gtk-4.0
 
 # Add udev rules for adb/fastboot 
-curl https://raw.githubusercontent.com/M0Rf30/android-udev-rules/main/51-android.rules | sudo tee /etc/udev/rules.d/51-android.rules; chmod +r /etc/udev/rules.d/51-android.rules
+curl https://raw.githubusercontent.com/M0Rf30/android-udev-rules/main/51-android.rules | sudo tee /etc/udev/rules.d/51-android.rules; sudo chmod +r /etc/udev/rules.d/51-android.rules
 
 echo "Please rebase to a ublue image ublue.it/images/ (pick either silverblue-main or silverblue-nvidia) based on the dedicated gpu you have."
 echo "If you need more information on the other spins visit the ublue.it website."
