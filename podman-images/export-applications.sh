74f8e3f5076257b4e4277245ef1bdb40efe4b295
John Ghatas <johnghatas97@hotmail.com>
2023-12-01 17:21:25 +0100

    fix: Remove exporting in the .ini file for now, restore the export script

---
+++ podman-images/export-applications.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+CONTAINER=(general rstudio ubuntu development)
+
+# All binaries and apps from each container
+APPS_GENERAL=(thunderbird brave-browser)
+APPS_RSTUDIO=(rstudio)
+APPS_UBUNTU=(intellij-idea-community)
+APPS_DEVELOPMENT=(codium)
+
+BIN_DEVELOPMENT=(/usr/bin/jq /usr/bin/tig /usr/bin/ansible-playbook /usr/bin/ansible-galaxy /usr/local/bin/kind)
+
+
+for env in "${CONTAINER[@]}"
+do
+	echo "Exporting the apps for $env"
+	declare -n APPS_ARR=APPS_${env^^}
+	if [ -n "${!APPS_ARR}" ]; then
+		echo "Found applications to export for $env"
+		for application in "${APPS_ARR[@]}"; do distrobox-enter -n $env -- distrobox-export --app $application -d; done
+		for application in "${APPS_ARR[@]}"; do distrobox-enter -n $env -- distrobox-export --app $application; done
+	fi
+
+	echo "Exporting the apps for $env"
+	declare -n BIN_ARR=BIN_${env^^}
+	if [ -n "${!BIN_ARR}" ]; then
+		echo "Found binaries to export for $env"
+		for binary in "${BIN_ARR[@]}"; do distrobox-enter -n $env -- distrobox-export --bin $binary --export-path ~/.local/bin -d; done
+		for binary in "${BIN_ARR[@]}"; do distrobox-enter -n $env -- distrobox-export --bin $binary --export-path ~/.local/bin; done
+	fi
+done
