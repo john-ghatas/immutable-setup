0740c142883043e530836951578fb104315bdbd1
John Ghatas <johnghatas97@hotmail.com>
2024-04-09 22:57:57 +0200

    chore(distrobox): Add nmap to the exported applications

--- podman-images/export-applications.sh
+++ podman-images/export-applications.sh
@@ -7,10 +7,7 @@ APPS_RSTUDIO=(rstudio)
 APPS_UBUNTU=(intellij-idea-community)
 APPS_DEVELOPMENT=(codium)
 
-BIN_DEVELOPMENT=(/usr/bin/jq /usr/bin/nc /usr/bin/tig /usr/bin/ansible-playbook /usr/bin/ansible-galaxy /usr/local/bin/kind /usr/local/bin/kubectl /usr/local/bin/devbox)
-
-# Install devbox on the local user
-distrobox enter development -- 'bash -c "$(curl -fsSL https://get.jetpack.io/devbox) -f"'
+BIN_DEVELOPMENT=(/usr/bin/jq /usr/bin/nmap /usr/bin/nc /usr/bin/tig /usr/bin/ansible-playbook /usr/bin/ansible-galaxy /usr/local/bin/kind /usr/local/bin/kubectl)
 
 for env in "${CONTAINER[@]}"
 do
@@ -30,3 +27,10 @@ do
 		for binary in "${BIN_ARR[@]}"; do distrobox-enter -n $env -- distrobox-export --bin $binary --export-path ~/.local/bin; done
 	fi
 done
+
+echo "-----------------------------------------------------------------------"
+echo "All, done to install and export devbox please run the following commands:"
+echo "distrobox enter development" 
+echo "curl -fsSL https://get.jetify.com/devbox | bash" 
+echo "distrobox-export --bin /usr/local/bin/devbox --export-path ~/.local/bin" 
+echo "-----------------------------------------------------------------------"
