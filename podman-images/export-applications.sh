f5a7a9d3a4742d2e0bac05aed7bfdc8f03c98e97
John Ghatas <johnghatas97@hotmail.com>
2024-08-24 23:14:04 +0200

    feature: Add Ansible to the export script

--- podman-images/export-applications.sh
+++ podman-images/export-applications.sh
@@ -6,7 +6,7 @@ APPS_GENERAL=(thunderbird)
 APPS_RSTUDIO=(rstudio)
 APPS_UBUNTU=(intellij-idea-community)
 APPS_DEVELOPMENT=(codium)
-BIN_DEVELOPMENT=(/usr/bin/jq /usr/bin/nmap /usr/bin/nc /usr/bin/tig /usr/bin/ansible-playbook /usr/bin/ansible-galaxy)
+BIN_DEVELOPMENT=(/usr/bin/jq /usr/bin/nmap /usr/bin/nc /usr/bin/tig /usr/bin/ansible /usr/bin/ansible-playbook /usr/bin/ansible-galaxy)
 
 for env in "${CONTAINER[@]}"
 do
