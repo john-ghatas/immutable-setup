ded7a022b778fcefdcffb032cda674c10a06351e
John Ghatas <johnghatas97@hotmail.com>
2025-12-19 21:04:44 +0100

    feat: update packages and remove the devpod install

--- Silverblue/post-rebase.sh
+++ Silverblue/post-rebase.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Setting up packages" 
-rpm-ostree install tilix gnupg2-scdaemon gnupg2-smime
+rpm-ostree install gnupg2-smime libnfc libnfc-examples neovim tilix
 
 # Add udev rules for hardware keys
 sudo wget https://raw.githubusercontent.com/Yubico/libfido2/refs/heads/main/udev/70-u2f.rules -O /etc/udev/rules.d/70-u2f.rules && sudo chmod 774 /etc/udev/rules.d/70-u2f.rules
@@ -17,9 +17,6 @@ flatpak override --user --filesystem=$HOME/.themes --filesystem=$HOME/.icons --f
 # Init GPG and PCSCd
 systemctl --user enable --now  gpg-agent.socket gpg-agent-extra.socket  gpg-agent-ssh.socket
 sudo systemctl enable --now pcscd.service pcscd.socket
-
-# Install Devpod on the current user account
-curl -L -o ~/.local/bin/devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && chmod 755 ~/.local/bin/devpod
 
 # Add udev rules for adb/fastboot and usevia.app 
 curl https://raw.githubusercontent.com/M0Rf30/android-udev-rules/main/51-android.rules | sudo tee /etc/udev/rules.d/51-android.rules; sudo chmod +r /etc/udev/rules.d/51-android.rules
