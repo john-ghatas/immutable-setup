16dd642592641308ceb4aa0af72f9d8b658454a3
John Ghatas <johnghatas97@hotmail.com>
2025-12-19 21:38:40 +0100

    feat: restructure the compose.ini files and deprecate the export script

--- Silverblue/post-rebase.sh
+++ Silverblue/post-rebase.sh
@@ -18,6 +18,10 @@ flatpak override --user --filesystem=$HOME/.themes --filesystem=$HOME/.icons --f
 systemctl --user enable --now  gpg-agent.socket gpg-agent-extra.socket  gpg-agent-ssh.socket
 sudo systemctl enable --now pcscd.service pcscd.socket
 
+# Install DevBox
+curl -L -o ~/.local/bin/devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && chmod 755 ~/.local/bin/devpod
+
+
 # Add udev rules for adb/fastboot and usevia.app 
 curl https://raw.githubusercontent.com/M0Rf30/android-udev-rules/main/51-android.rules | sudo tee /etc/udev/rules.d/51-android.rules; sudo chmod +r /etc/udev/rules.d/51-android.rules
 
