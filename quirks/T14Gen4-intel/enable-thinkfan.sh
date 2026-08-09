4cf365a2747333f58954159f4d76bd73c93dc23a
John Ghatas <john@johnghatas.com>
2026-08-09 13:03:25 +0200

    feat(t14gen4): prepare for the cutover to power-profiles-daemon

--- quirks/T14Gen4-intel/enable-thinkfan.sh
+++ quirks/T14Gen4-intel/enable-thinkfan.sh
@@ -3,9 +3,19 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPT_DIR"
 
-# Disable thermald and enabled tuned
-echo "Disable thermald and enabled tuned"
-sudo systemctl disable --now thermald
+# Setup thermald and tuned
+echo "Setup thermald and tuned"
+
+sudo mkdir -p /etc/systemd/system/thermald.service.d
+sudo echo -e "[Service]\nExecStart=\nExecStart=/usr/sbin/thermald --no-daemon --dbus-enable --ignore-cpuid-check --exclusive-control" | sudo tee /etc/systemd/system/thermald.service.d/override.conf && sudo systemctl daemon-reload
+
+sudo mkdir -p /etc/thermald
+sudo echo -e '<?xml version="1.0"?><ThermalConfiguration></ThermalConfiguration>' | sudo tee /etc/thermald/thermal-conf.xml
+
+sudo systemctl daemon-reload
+sudo systemctl restart thermald.service
+
+sudo systemctl enable --now powertop.service
 sudo systemctl enable --now tuned
 
 sudo mkdir -p /etc/tuned
@@ -16,12 +26,22 @@ sleep_interval = 1
 update_interval = 1
 EOF
 
+# Create directories
 sudo mkdir -p /etc/tuned/profiles/{thinkpad-battery,thinkpad-powersave}
+sudo mkdir -p /etc/systemd/system-sleep
+
+# Copy the profiles
 sudo cp tuned/tuned-balanced.conf /etc/tuned/profiles/thinkpad-battery/tuned.conf
 sudo cp tuned/tuned-powersave.conf /etc/tuned/profiles/thinkpad-powersave/tuned.conf
-
 sudo cp tuned/ppd.conf /etc/tuned/ppd.conf
 
+# Install the sleep scripts
+sudo cp tuned/tuned-wake-fix /etc/systemd/system-sleep/tuned-wake-fix
+sudo chmod +x /etc/systemd/system-sleep/tuned-wake-fix
+sudo cp tuned/tuned-wake-fix.service /etc/systemd/system/tuned-wake-fix.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable tuned-wake-fix.service
 sudo systemctl restart tuned
 
 # Setting up thinkfan
